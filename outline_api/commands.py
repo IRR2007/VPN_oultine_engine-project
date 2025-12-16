e31bb35abfbb61e7ba09ad602346c3bae8ed8a87
Fizuli Ragimov <fizuli20082005@gmail.com>
2025-12-16 11:53:27 +0300

    Added return in functions and added types

--- outline_api/commands.py
+++ outline_api/commands.py
@@ -1,11 +1,12 @@
 import asyncio
 from config import logging
+from typing import Any, List
 
-def bytes_to_gb(data):
+def bytes_to_gb(data) -> int:
 	return data / 1024 / 1024 / 1024
 
 class OutlineCommands():
-	def __init__(self, client):
+	def __init__(self, client) -> None:
 		self.client = client
 
 	async def generate_new_key(self, username):
@@ -17,25 +18,23 @@ class OutlineCommands():
 			logging.info(f"Error while generating: {e}")
 			return None
 
-	async def delete_key(self, key):
+	async def delete_key(self, key) -> None:
 		if key is not None:
 			try:
 				await asyncio.to_thread(self.client.delete_key, key.key_id)
-				print(f"Deleted key {key.name}")
+				logging.info(f"Deleted key {key.name}")
 			except Exception as e:
 				logging.info(f"Error while deleting key {key.name}: {e}")
 
-	async def get_keys_info(self):
+	async def get_keys_info(self) -> List[Any]:
 		try:
 			keys = await asyncio.to_thread(self.client.get_keys)
 			for key in keys:
-				print(f"NAME: {key.name}")
-				print(f"ID: {key.key_id}")
-				print(f"URL: {key.access_url}")
+				print(f"NAME: {key.name}\nID: {key.key_id}\nURL: {key.access_url}")
 				try:
-					print(f"DATA USED: {bytes_to_gb(int(key.used_bytes))} GB")
+					print(f"DATA USED: {bytes_to_gb(int(key.used_bytes))} GB\n")
 				except:
-					print(f"DATA USED: {key.used_bytes}")
-				print()
+					print(f"DATA USED: {key.used_bytes}\n")
+			return keys
 		except Exception as e:
 			logging.info(f"Error while getting information: {e}")
