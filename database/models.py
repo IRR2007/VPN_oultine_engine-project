cd85fcd837e9061ba3ece1ee9355475314cea324
Fizuli Ragimov <fizuli20082005@gmail.com>
2025-12-21 22:54:27 +0300

    HOT FIX

--- database/models.py
+++ database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import (Column,
-                        Integer, String, DateTime, func, Index)
+                        Integer, String, Date, func, Index)
 
 Base = declarative_base()
 
@@ -8,10 +8,11 @@ Base = declarative_base()
 class Key(Base):
     __tablename__ = 'key'
     id = Column(Integer, primary_key=True)
+    outline_id = Column(String, nullable=False)
     access_url = Column(String, nullable=False)
     user = Column(String, nullable=False)
-    creation_date = Column(DateTime, nullable=False, server_default=func.now())
-    expiration_date = Column(DateTime(timezone=True), nullable=False)
+    creation_date = Column(Date, nullable=False, server_default=func.now())
+    expiration_date = Column(Date, nullable=False)
 
     __table_args__ = (
         Index('idx_key', 'access_url'),
